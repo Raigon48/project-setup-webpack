c007b4954ea5d1430331b2e289a023f5d9bce578
Abhishek_Rai <raigon48@gmail.com>
2020-06-02 23:48:51 +0530

    output main.js with different name

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,11 @@ module.exports = {
     // this is the configuration used by webpack
     // mode is used to setup environment : development/production
     mode : "development",
-    entry : "./src/engine.js"
+    // provides the entry point for the webpack for bundling project
+    entry : "./src/engine.js",
+    // provides the control over the output
+    output : {
+        filename : "build.js"
+    }
+
 }
