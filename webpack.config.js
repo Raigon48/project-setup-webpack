f528374d9ccdd9d0bb1c695f285734eef389d06f
Abhishek_Rai <raigon48@gmail.com>
2020-06-03 01:38:06 +0530

    adding placeholder

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 var path = require('path');
 
+/*
+[hash]
+[chunkhash]
+[name]
+[id]
+[query]
+[contenthash]
+*/
+
 module.exports = {
     // this is the configuration used by webpack
     // mode is used to setup environment : development/production
     mode : "development",
     // provides the entry point for the webpack for bundling project
-    entry : "./src/engine.js",
+    entry : {
+        myfile : './src/engine.js'
+    },
     // provides the control over the output
     output : {
         path : path.resolve(__dirname, 'build'),
-        filename : "build.js"
+        filename : "js/[name].[contenthash].js"
     }
 
 }
